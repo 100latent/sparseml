8c3fdcd35bf904980bfb42b69b926e9eb1967198
Marcus G K Williams <Marcus G K Williams 168222+mgkwill@users.noreply.github.com>
2025-05-27 18:39:19 -0700

    Fix uv sync
    Signed-off-by: Marcus G K Williams <Marcus G K Williams 168222+mgkwill@users.noreply.github.com>

--- src/sparseml/version.py
+++ src/sparseml/version.py
@@ -16,7 +16,8 @@
 Functionality for storing and setting the version info for SparseML
 """
 
-version_base = "1.8.0"
+version_base = "1.8.1"
+__version__ = version_base
 is_release = False  # change to True to set the generated version as a release version
 is_dev = False
 dev_number = None
